2504be9a5473805ee3ace406d34634e2fe9dbfe5
debugger77 <91728643+debugger77@users.noreply.github.com>
2023-09-27 20:14:07 +0000

    compare

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,9 +8,9 @@
 // console.log("2" > 1);
 // console.log("02" > 1);     // Try to compare values with same datatype
 
-//console.log(null > 0);   //equality check(==) and comparision operators(<,>,<=,>=) work differently
-//console.log(null == 0);   // comparisions convert null to a number, treating it as 0
-//console.log(null >= 0);   // that is why null > 0 gives false and null >= 0 gives true. 
+console.log(null > 0);   //equality check(==) and comparision operators(<,>,<=,>=) work differently
+console.log(null == 0);   // comparisions convert null to a number, treating it as 0
+console.log(null >= 0);   // that is why null > 0 gives false and null >= 0 gives true. 
 
 // console.log(undefined == 0);
 // console.log(undefined > 0);         // undefined gives false in all cases
@@ -18,4 +18,4 @@
 
 // ===              (strict check -> it checks the values along with datatypes)
 
-// console.log("2" === 2);
+console.log("2" === 2);
