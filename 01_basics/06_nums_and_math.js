2588ba6b68f164b8b14274eb30a50fbf452ce8de
debugger77 <91728643+debugger77@users.noreply.github.com>
2023-09-29 10:40:36 +0000

    num and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.55)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(1));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);   // To avoid 0, 1 is added.
+
+const min = 20
+const max = 30
+
+// To generate values in the given range.
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
