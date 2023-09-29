7b7d814a257bdb24ec446c043ad37c5a075df9e1
debugger77 <91728643+debugger77@users.noreply.github.com>
2023-09-29 12:30:12 +0000

    date and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -16,15 +16,15 @@ console.log(myCreatedDate.toString());
 let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myCreatedDate.getTime());           // Normally time is in milliseconds
+console.log(Math.floor(Date.now() / 1000));       // To get the time in seconds
 
 let newDate = new Date()
 console.log(newDate.toString());
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+console.log((`The day is ${newDate.getDay()} and the month is ${newDate.getMonth() + 1}`))
 
 newDate.toLocaleString('default', {
     weekday: "long",
