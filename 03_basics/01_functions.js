8b8d64dc5d51c58495d42489fbbf0993fe0ae3b0
debugger77 <91728643+debugger77@users.noreply.github.com>
2023-09-30 18:24:08 +0000

    taking parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,7 +43,7 @@ function calculateCartPrice(val1, val2, ...num1) { //here the spread operator is
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+console.log(calculateCartPrice(200, 400, 500, 2000))         // answer -> [ 500, 2000 ]
 
 const user = {
     username: "harshil",
