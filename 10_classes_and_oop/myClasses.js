084119489b92d841d7f2855fae26467b3a728549
debugger77 <91728643+debugger77@users.noreply.github.com>
2023-10-16 18:24:32 +0000

    class and constructor

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,41 @@
+// ES6
+
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const js = new User("js", "js@gmail.com", "123")
+
+// console.log(js.encryptPassword());
+// console.log(js.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const obj = new User("obj", "obj@gmail.com", "123")
+
+console.log(obj.encryptPassword());
+console.log(obj.changeUsername());
